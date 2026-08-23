793529c0b3f2556197eefc1908b179196fcac067
adrija-03 <adrijamitra01@gmail.com>
2026-08-23 23:32:55 +0530

    Toggle button implemented

--- 02-toggle-button/script.js
+++ 02-toggle-button/script.js
@@ -1,4 +1,10 @@
 const toggleBtn = document.querySelector("#toggle-btn")
-toggleBtn.addEventListener('click', () => {
-    console.log("Hello world")
+const background = document.querySelector("#background")
+toggleBtn.addEventListener('click', (e) => {
+    let bgColor = e.currentTarget.parentNode.classList.value;
+    if(bgColor === "light-theme") {
+        e.currentTarget.parentNode.classList.replace("light-theme", "dark-theme")
+    } else if(bgColor === "dark-theme") {
+        e.currentTarget.parentNode.classList.replace("dark-theme", "light-theme")
+    }
 })
