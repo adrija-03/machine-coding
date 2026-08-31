132da9c49b094a62f6a034a19ae5289482c9ee08
adrija-03 <adrijamitra01@gmail.com>
2026-08-31 16:23:18 +0530

    Accordian functionality implemented

---
+++ accordian/src/components/Accordian2.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+function Accordian2() {
+    const [visibility, setVisibility] = useState([false, false, false])
+
+    function handleClick(e) {
+        let key = Number(e.currentTarget.id);
+        setVisibility(visibility.map((ele, x) => {
+            if (key === x) {
+                return !ele;
+            }
+            return false;
+        }))
+    }
+
+    return (
+        <>
+            <div>
+                <div id="0" onClick={handleClick} style={{ cursor: "pointer" }}>Section 1</div>
+                <p style={{ display: visibility[0] ? "block" : "none" }}>lorem ipsum</p>
+            </div>
+
+            <div>
+                <div id="1" onClick={handleClick} style={{ cursor: "pointer" }}>Section 2</div>
+                <p style={{ display: visibility[1] ? "block" : "none" }}>lorem ipsum</p>
+            </div>
+
+            <div>
+                <div id="2" onClick={handleClick} style={{ cursor: "pointer" }}>Section 3</div>
+                <p style={{ display: visibility[2] ? "block" : "none" }}>lorem ipsum</p>
+            </div>
+        </>
+    )
+}
+
+export default Accordian2
