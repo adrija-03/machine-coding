f187059638f937524f760a84358a4806a51c676b
adrija-03 <adrijamitra01@gmail.com>
2026-09-16 23:59:42 +0530

    progress bar

---
+++ progress-bar/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ProgressBar from './components/ProgressBar'
+
+function App() {
+
+  return (
+    <div className='h-[100vh]'>
+      <ProgressBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ progress-bar/src/components/ProgressBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+function ProgressBar() {
+    const [progress, setProgress] = useState(0)
+
+    function handleProgress(amt) {
+        setProgress(prev => prev + amt)
+    }
+
+    console.log(progress)
+
+    return (
+        <div className="flex justify-center items-center gap-5 h-full m-5">
+            <div className="w-full h-6 bg-[#eee] rounded-full">
+                <div
+                    className="bg-[#00FF00] rounded-full h-full"
+                    style={{ width: `${progress}%` }}></div>
+            </div>
+            <button
+                className="rounded-full bg-cyan-500 w-[5%] cursor-pointer"
+                onClick={() => handleProgress(10)}
+                disabled={progress === 100}
+            >
+                +10
+            </button>
+            <button
+                className="rounded-full bg-cyan-500 w-[5%] cursor-pointer"
+                onClick={() => handleProgress(-10)}
+                disabled={progress === 0}
+            >
+                -10
+            </button>
+        </div>
+    )
+}
+
+export default ProgressBar
