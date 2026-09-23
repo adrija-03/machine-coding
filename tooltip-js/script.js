8ab2d2415781f7e459ff7ebcdc5ecbaf2c747178
adrija-03 <adrijamitra01@gmail.com>
2026-09-23 16:19:15 +0530

    Tooltip designed

---
+++ tooltip-js/script.js
@@ -0,0 +1,14 @@
+const content = document.querySelector("#content")
+const hoverBtn = document.querySelector("#hover-btn")
+
+const tooltip = document.createElement('span');
+tooltip.textContent = "Hi! I am a tooltip";
+
+hoverBtn.addEventListener('mouseenter', (e) => {
+    
+    content.appendChild(tooltip)
+})
+
+hoverBtn.addEventListener('mouseleave', (e) => {
+    content.removeChild(tooltip);
+})
